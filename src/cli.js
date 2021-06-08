5d7ae92e6f44f01f6176b887944fc4e1feb9972d
Andrey Listochkin <listochkin@hey.com>
2021-06-08 19:05:20 +0300

    login form submission

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 import { startServer } from './server.js';
 
 const port = process.env.PORT || 2021;
+const password = process.env.PASSWORD || 'letmeupload';
 
-startServer(port);
+startServer(port, password);
--- src/server.js
+++ src/server.js
@@ -3,7 +3,9 @@ import { networkInterfaces } from 'os';
 import { readFile } from 'fs/promises';
 import { exec } from 'child_process';
 
-export function startServer(port) {
+let uploadPassword;
+
+export function startServer(port, password) {
   console.log('Starting server on port ' + port);
   server.listen(port, () => {
     Object.entries(networkInterfaces()).forEach(([name, interfaces]) => {
@@ -11,6 +13,7 @@ export function startServer(port) {
         .filter(iRecord => iRecord.family === 'IPv4')
         .map(iRecord => console.log(`http://${iRecord.address}:${port}`));
     });
+    uploadPassword = password;
   });
 
   setTimeout(() => {
@@ -19,7 +22,35 @@ export function startServer(port) {
 }
 
 const server = http.createServer(async (request, response) => {
+  if (request.method === 'POST' && request.url === '/login') {
+    await performLogin(request, response);
+  } else if (request.method === 'GET') {
+    await serveContent(request, response);
+  } else {
+    response.statusCode = 400;
+    response.end();
+  }
+});
+
+async function performLogin(request, response) {
+  let body = '';
+  for await (const chunk of request) body += chunk;
+  const formData = new URLSearchParams(body);
+  const userPassword = formData.get('password');
+  if (userPassword === uploadPassword) {
+    // TODO save session
+
+    response.statusCode = 302;
+    response.setHeader('Location', `${request.headers.referer}`);
+    response.end();
+  } else {
+    response.statusCode = 401;
+    response.end();
+  }
+}
+
+async function serveContent(request, response) {
   const html = await readFile(new URL('./index.html', import.meta.url));
   response.write(html);
   response.end();
-});
+}
